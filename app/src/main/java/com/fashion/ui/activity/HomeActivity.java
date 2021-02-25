5617cafdbce93743cf1a9c48ad8b7674b4a6b642
hedinang <tridunga1@gmail.com>
2021-02-26 00:27:46 +0700

    a

--- app/src/main/java/com/fashion/ui/activity/HomeActivity.java
+++ app/src/main/java/com/fashion/ui/activity/HomeActivity.java
@@ -16,18 +16,19 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-//        FloatingActionButton fab = findViewById(R.id.fab);
-//        LinearLayout linearLayout = findViewById(R.id.viewB);
-//
-//        fab.setOnClickListener(v -> {
-//            if (slide == false) {
-//                linearLayout.setVisibility(View.GONE);
-//                slide = true;
-//            } else {
-//                linearLayout.setVisibility(View.INVISIBLE);
-//                slide = false;
-//            }
-//        });
+        FloatingActionButton fab = findViewById(R.id.fab);
+        LinearLayout linearLayout = findViewById(R.id.ll_body);
+
+        fab.setOnClickListener(v -> {
+            if (slide == false) {
+                linearLayout.setVisibility(View.GONE);
+                slide = true;
+            } else {
+                linearLayout.setVisibility(View.VISIBLE);
+                slide = false;
+            }
+        });
     }
+
 
 }
--- app/src/main/java/com/fashion/ui/activity/TestActivity.java
+++ app/src/main/java/com/fashion/ui/activity/TestActivity.java
@@ -1,6 +1,7 @@
 package com.fashion.ui.activity;
 
 import android.view.View;
+import android.widget.LinearLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.fashion.R;
@@ -8,11 +9,23 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 public class TestActivity extends AppCompatActivity {
+    public Boolean slide = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        LinearLayout linearLayout = findViewById(R.id.viewB);
 
+        fab.setOnClickListener(v -> {
+            if (slide == false) {
+                linearLayout.setVisibility(View.GONE);
+                slide = true;
+            } else {
+                linearLayout.setVisibility(View.INVISIBLE);
+                slide = false;
+            }
+        });
     }
 }
